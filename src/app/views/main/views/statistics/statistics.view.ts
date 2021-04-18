3bc25ff5ce091cf4ca23c988438df31e0b987c6d
rado.poghossian98 <rado.poghossian98@gmail.com>
2021-04-18 19:47:30 +0400

    create chart

--- src/app/views/main/views/statistics/statistics.view.ts
+++ src/app/views/main/views/statistics/statistics.view.ts
@@ -20,14 +20,13 @@ export class StatisticsViewComponent implements OnInit, AfterViewInit, OnDestroy
 
     private _initChart(): void {
         const canvasElement: any = document.getElementById('chart');
-        console.log(canvasElement);
         this._chart = new Chart(canvasElement, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['Strategi 1', 'Strategi 2', 'Strategi 3', 'Strategi 4', 'Strategi 5', 'Strategi 6', 'Strategi 7', 'Strategi 8'],
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [12, 19, 3, 5, 2, 3, 8, 9],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -48,9 +47,69 @@ export class StatisticsViewComponent implements OnInit, AfterViewInit, OnDestroy
                 }]
             },
             options: {
-                scales: {
-                    
-                }
+            }
+        });
+
+
+
+        const canvasElement2: any = document.getElementById('chart2');
+        this._chart = new Chart(canvasElement2, {
+            type: 'bar',
+            data: {
+                labels: ['Strategi 1', 'Strategi 2', 'Strategi 3', 'Strategi 4'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3, 8, 9],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+            }
+        });
+        const canvasElement3: any = document.getElementById('chart3');
+        this._chart = new Chart(canvasElement3, {
+            type: 'bar',
+            data: {
+                labels: ['Strategi 1', 'Strategi 2', 'Strategi 3', 'Strategi 4'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3, 8, 9],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
             }
         });
     }
